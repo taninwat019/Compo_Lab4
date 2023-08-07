dfa193d6349f72fab5b59d47a86edc8a4c236439
Taninwat <fame5454@gmail.com>
2023-08-07 21:14:25 +0700

    1.4

--- real-world-vue/src/views/event/EventRegisterView.vue
+++ real-world-vue/src/views/event/EventRegisterView.vue
@@ -1,14 +1,26 @@
 <script setup  lang="ts">
 import type { EventItem } from '@/type'
 import type { PropType } from 'vue';
+import { useRouter } from 'vue-router'
 
-defineProps({
+const props = defineProps({
     event: {
         type: Object as PropType<EventItem>,
-            require:true
+        require:true
     }
 })
+
+const router = useRouter()
+function register() {
+    router.push({
+        name: 'event-detail',
+        params: {
+            id: props.event?.id
+        }
+    })
+}
 </script>
 <template>
        <p> Registration from here</p>
+       <button @click="register"> Register Me </button>
 </template>
